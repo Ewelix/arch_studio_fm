b0645c0d82e65d80460708a9c09456dd34adb753
Ewe <ewelina.polewka1@gmail.com>
2022-01-21 15:59:58 +0100

    Content displayed on About section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { pageTextContent } from '../../data/pageTextContent';
 
 const About = () => {
-  return <div>hi</div>;
+  const { leadersList: { name, position } } = pageTextContent;
+  return (
+    <section>
+      <div>
+        <h2>Your team of professionals</h2>
+        <p>Our small team of world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationship our projects have with their surroundings.</p>
+      </div>
+      <div>
+        <div>
+          <h2>Our Heritage</h2>
+          <p>Founded in 2007, we started as a trio of architects. Our complimentary skills and relentless attention to detail turned Arch into one of the most sought after boutique firms in the country.</p>
+          <p>Speciliazing in Urban Design allowed us to focus on creating exceptional structures that live in harmony with their surroundings. We consider every detail from every surrounding element to inform our designs. </p>
+          <p>Our small team of world-class professionals provides input on every project.</p>
+        </div>
+        <img src='' alt='' />
+        </div>
+      <div>
+        <h2>The Leaders</h2>
+        {pageTextContent.leadersList.map((leader) => {
+          return (
+            <div>
+              <img src='' alt='' />
+              <p>{leader.name}</p>
+              <p>{leader.position}</p>
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/data/pageTextContent.js
+++ src/data/pageTextContent.js
@@ -1,113 +1,84 @@
 export const pageTextContent = {
-  portfolio: [
+  projects: [
     {
       name: 'Seraph Station',
       date: 'September 2019',
-      imageName: 'image-seraph'
     },
     {
       name: 'Eebox Building',
-      date: 'August 2017'
+      date: 'August 2017',
     },
     {
       name: 'Federal II Tower',
-      date: 'March 2017'
+      date: 'March 2017',
     },
     {
       name: 'Project Del Sol',
-      date: 'January 2016'
+      date: 'January 2016',
     },
     {
       name: 'Le Prototype',
-      date: 'October 2015'
+      date: 'October 2015',
     },
     {
       name: '228B Tower',
-      date: 'April 2015'
+      date: 'April 2015',
     },
     {
       name: 'Grand Edelweiss Hotel',
-      date: 'December 2013'
+      date: 'December 2013',
     },
     {
       name: 'Netcry Tower',
-      date: 'August 2012'
+      date: 'August 2012',
     },
     {
       name: 'Hypers',
-      date: 'January 2012'
+      date: 'January 2012',
     },
     {
       name: 'SXIV Tower',
-      date: 'March 2011'
+      date: 'March 2011',
     },
     {
       name: 'Trinity Bank Tower',
-      date: 'September 2010'
+      date: 'September 2010',
     },
     {
       name: 'Project Paramour',
-      date: 'February 2008'
-    }
+      date: 'February 2008',
+    },
   ],
-  about: [
+  leadersList: [
     {
-      title: 'Your team of professionals',
-      introduction: 'Our small team of world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationship our projects have with their surroundings.',
+      name: 'Jake Richards',
+      position: 'Chief Architect',
     },
     {
-      title: 'Our \n' +
-        'Heritage',
-      main: 'Founded in 2007, we started as a trio of architects. Our complimentary skills and relentless attention to detail turned Arch into one of the most sought after boutique firms in the country.\n' +
-        '\n' +
-        'Speciliazing in Urban Design allowed us to focus on creating exceptional structures that live in harmony with their surroundings. We consider every detail from every surrounding element to inform our designs. \n' +
-        '\n' +
-        'Our small team of world-class professionals provides input on every project.'
+      name: 'Thompson Smith',
+      position: 'Head of Finance',
     },
     {
-      title: 'The \n' +
-        'Leaders',
-      leadersList: [
-        {
-          name: 'Jake Richards',
-          position: 'Chief Architect'
-        },
-        {
-          name: 'Thompson Smith',
-          position: 'Head of Finance'
-        },
-        {
-          name: 'Jackson Rourke',
-          position: 'Lead Designer'
-        },
-        {
-          name: 'Maria Simpson',
-          position: 'Senior Architect'
-        }
-      ]
-    }
+      name: 'Jackson Rourke',
+      position: 'Lead Designer',
+    },
+    {
+      name: 'Maria Simpson',
+      position: 'Senior Architect',
+    },
   ],
-  contact: {
-    headerTitle: 'Tell us about your project',
-    text: 'We’d love to hear more about your project. Please, leave a message below or give us a call. We have two offices, one in Texas and one in Tennessee. If you find yourself nearby, come say hello!',
-    mainTitle: 'Contact Details',
-    details: [
-      {
-        office: 'Main Office',
-        mail: 'Mail :		archone@mail.com',
-        address: 'Address :	1892  Chenoweth Drive TN',
-        phone: 'Phone : 123-456-3451',
-      },
-      {
-        office: 'Office II',
-        mail: 'Mail : archtwo@mail.com',
-        address: 'Address : 3399  Wines Lane TX',
-        phone: 'Phone : 832-123-4321',
-      }
-    ],
-    buttonText: 'View on Map',
-    footerTitle: 'Connect with us',
-
-    
-  }
-}
+  contactDetails: [
+    {
+      office: 'Main Office',
+      mail: 'Mail :		archone@mail.com',
+      address: 'Address :	1892  Chenoweth Drive TN',
+      phone: 'Phone : 123-456-3451',
+    },
+    {
+      office: 'Office II',
+      mail: 'Mail : archtwo@mail.com',
+      address: 'Address : 3399  Wines Lane TX',
+      phone: 'Phone : 832-123-4321',
+    },
+  ],
+};
