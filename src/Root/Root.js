d642486b7a1f2d42682814f68a13f2a3a3693927
Ewe <ewelina.polewka1@gmail.com>
2022-03-03 16:43:17 +0100

    Refactor

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
-import '../App.css';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../assets/styles/globalStyle';
 import { theme } from '../assets/styles/theme';
